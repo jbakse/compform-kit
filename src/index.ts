ef1e70bab8bed3c766b61c238dc6d4083e37b2ac
Justin Bakse <justin@tsfim.com>
2019-11-22 11:36:17 -0500

    add helloKit()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+function helloKit() {
+  return "Hello, Comp Form!";
+}
+
 function randomRange(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
+
+export { helloKit, randomRange };
