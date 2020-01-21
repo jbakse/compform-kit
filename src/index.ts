87bb89ef8a8f66a7432c4bbc5697f6caed7aaeab
Justin Bakse <justin@tsfim.com>
2020-01-21 11:59:24 -0500

    format code

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,9 @@ function randomRange(min: number, max: number): number {
 function mod(dividend: number, divisor: number): number {
   return dividend - divisor * Math.floor(dividend / divisor);
 }
+
 function lerp(min: number, max: number, n: number) {
-  return min * (1 - n) + (max * n);
+  return min * (1 - n) + max * n;
 }
 
 export { helloKit, randomRange, mod, lerp };
